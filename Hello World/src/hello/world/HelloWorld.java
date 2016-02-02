61b24e22fc9b60112bcdca6e8e649eea046a4e58
revans21 <raevans2@oakland.edu>
2016-02-02 11:59:27 -0500

    Added Author Comment

--- Hello World/src/hello/world/HelloWorld.java
+++ Hello World/src/hello/world/HelloWorld.java
@@ -9,13 +9,15 @@ package hello.world;
  *
  * @author Reamus
  */
+
+// author
 public class HelloWorld {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World ");
     }
     
 }
